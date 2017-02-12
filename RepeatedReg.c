b68c3901eb81bc7863f4f6126a830d1e17ad91aa
pr13051992 <npraveen.kumar2015@vit.ac.in>
2017-02-12 12:48:56 +0530

    Create RepeatedReg.c

---
+++ RepeatedReg.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+	int n;
+	printf("Enter the array size");
+	scanf("%d",&n);
+	int a[n];
+	int count=0;
+	printf("Enter the elements");
+	for(int i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	for(int i=0;i<n;i++){
+		for(int j=i+1;j<n;j++){
+			if(a[i]==a[j]){
+					printf("The number %d is repeated \n",a[i]);
+			}
+		}
+	}
+	
+	return 0;
+}
